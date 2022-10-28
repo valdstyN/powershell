30572074ddac1f6df3a81135f57b0125702781ba
valdstyN <valdstyn@gmail.com>
2022-10-28 21:16:17 +0200

    Add files via upload

---
+++ cmdline_s4hana_api.ps1
@@ -0,0 +1,103 @@
+# to be updated - add a mapping table between modules and entities (currently only working for Projects-related entities)
+# sample input: hana -method 'get' -entity 'ProjectSet' -select 'ProjectID,OrgID' -top 10
+
+CLS
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force
+$session = new-object microsoft.powershell.commands.webrequestsession
+
+# function format-json taken from source:
+# https://stackoverflow.com/questions/56322993/proper-formating-of-json-using-powershell/56324939#56324939
+function Format-Json {
+    [CmdletBinding(DefaultParameterSetName = 'Prettify')]
+    Param(
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+        [string]$Json,
+
+        [Parameter(ParameterSetName = 'Minify')]
+        [switch]$Minify,
+
+        [Parameter(ParameterSetName = 'Prettify')]
+        [ValidateRange(1, 1024)]
+        [int]$Indentation = 4,
+
+        [Parameter(ParameterSetName = 'Prettify')]
+        [switch]$AsArray
+    )
+    if ($PSCmdlet.ParameterSetName -eq 'Minify') {
+        return ($Json | ConvertFrom-Json) | ConvertTo-Json -Depth 100 -Compress
+    }
+    if ($Json -notmatch '\r?\n') {
+        $Json = ($Json | ConvertFrom-Json) | ConvertTo-Json -Depth 100
+    }
+    $indent = 0
+    $regexUnlessQuoted = '(?=([^"]*"[^"]*")*[^"]*$)'
+    $result = $Json -split '\r?\n' |
+        ForEach-Object {
+            if ($_ -match "[}\]]$regexUnlessQuoted") {
+                $indent = [Math]::Max($indent - $Indentation, 0)
+            }
+            $line = (' ' * $indent) + ($_.TrimStart() -replace ":\s+$regexUnlessQuoted", ': ')
+            if ($_ -match "[\{\[]$regexUnlessQuoted") {
+                $indent += $Indentation
+            }
+            $line
+        }
+    if ($AsArray) { return $result }
+    return $result -Join [Environment]::NewLine
+}
+
+# -- Q TENANT ---
+$user = 'S/4 API user here'
+$pwd = 'S/4 API password here'
+$baseUrl = 'https://myXXXXXX.s4hana.ondemand.com/sap/opu/odata/'
+$creds = $user+":"+$pwd
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($creds))
+$basicauth_header = "Basic $encodedCreds"
+$uri = $baseUrl + 'cpd/SC_PROJ_ENGMT_CREATE_UPD_SRV/ProjectSet?$top=1'
+
+# need a mapping table so we don't have to indicate the module (only the entity)
+
+function hana($method,$entity,$select,$filter,$top,$body){
+
+    # [1] get the CSRF token
+    $headers = @{
+        'Authorization' = $basicauth_header
+        'X-CSRF-Token' = 'Fetch'
+        'Content-Type' = 'application/json'    
+    }
+    $r = Invoke-WebRequest -Method Get -Uri $uri -Headers $headers -WebSession $session
+    $token = $r.Headers['X-CSRF-Token']
+
+    # [2] set the token in the new request
+    $headers = @{
+        'Authorization' = $basicauth_header
+        'X-CSRF-Token' = $token
+        'Content-Type' = 'application/json'    
+        'Accept' = 'application/json'  
+        'DataServiceVersion' = '2.0'
+        # extra header required for internal projects
+    }
+
+    if($select.Length -ne 0){$select='$select='+$select}else{$select=""}
+    if($filter.Length -ne 0){$filter='$filter='+$filter}else{$filter=""}
+    if($top.Length -ne 0){$top='$top='+$top}else{$top=""}
+    
+    $uri = ($baseUrl+'cpd/SC_PROJ_ENGMT_CREATE_UPD_SRV/'+$entity+'?') + (($select,$filter,$top -join '&') -replace "&+","&")
+    write-host ">> Querying S/4HANA {$uri}"
+    $rq = Invoke-WebRequest -Method $method -Uri $uri -Headers $headers -Body $body -WebSession $session
+    write-host ($rq | Format-Json)
+}
+
+$q = "X"
+while($q.Length -ne 0){
+    $q = Read-Host '>> '
+    if($q.Length -gt 0) {
+        if($q -eq 'sample'){
+            $q =  "hana -method 'get' -entity 'ProjectSet' -select 'ProjectID,OrgID' -top 10"
+         }
+        Invoke-Expression $q
+    }
+}
+
+
+
